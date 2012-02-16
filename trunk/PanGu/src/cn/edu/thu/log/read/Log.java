1f475beb579fc8cf77c65c730aa0a235af083a45
DouMeng1988@gmail.com <DouMeng1988@gmail.com@28f5e624-9102-7313-13ba-88312b7d0dff>
2012-02-16 09:20:56 +0000

    So far: read each log(menu->new file) read log folder(menu->new file) append logs(menu->add file) merge logs(menu->new file->pick folder that contains logs with different tag)

---
+++ trunk/PanGu/src/cn/edu/thu/log/read/Log.java
@@ -0,0 +1,64 @@
+package cn.edu.thu.log.read;
+
+import java.util.ArrayList;
+
+public class Log {
+	/** tag */
+	ArrayList<String> logHeadTagList;
+	ArrayList<String> logBodyTagList;
+	ArrayList<String> logTagList;
+	/** content */
+	ArrayList<Object> logHeadContent;
+	ArrayList<Object> logBodyContent;
+	ArrayList<Object> logContent;
+
+	/** get and set functions */
+	public ArrayList<String> getLogHeadTagList() {
+		return logHeadTagList;
+	}
+
+	public void setLogHeadTagList(ArrayList<String> logHeadTagList) {
+		this.logHeadTagList = logHeadTagList;
+	}
+
+	public ArrayList<String> getLogBodyTagList() {
+		return logBodyTagList;
+	}
+
+	public void setLogBodyTagList(ArrayList<String> logBodyTagList) {
+		this.logBodyTagList = logBodyTagList;
+	}
+
+	public ArrayList<String> getLogTagList() {
+		return logTagList;
+	}
+
+	public void setLogTagList(ArrayList<String> logTagList) {
+		this.logTagList = logTagList;
+	}
+
+	public ArrayList<Object> getLogHeadContent() {
+		return logHeadContent;
+	}
+
+	public void setLogHeadContent(ArrayList<Object> logHeadContent) {
+		this.logHeadContent = logHeadContent;
+	}
+
+	public ArrayList<Object> getLogBodyContent() {
+		return logBodyContent;
+	}
+
+	public void setLogBodyContent(ArrayList<Object> logBodyContent) {
+		this.logBodyContent = logBodyContent;
+	}
+
+	public ArrayList<Object> getLogContent() {
+		return logContent;
+	}
+
+	public void setLogContent(ArrayList<Object> logContent) {
+		this.logContent = logContent;
+	}
+
+}
--- trunk/PanGu/src/cn/edu/thu/log/web/service/impl/LogReadServiceImpl.java
+++ trunk/PanGu/src/cn/edu/thu/log/web/service/impl/LogReadServiceImpl.java
@@ -2,10 +2,14 @@ package cn.edu.thu.log.web.service.impl;
 
 import java.io.File;
 import java.util.ArrayList;
+
+import cn.edu.thu.log.read.Log;
 import cn.edu.thu.log.read.LogConfig;
 import cn.edu.thu.log.read.LogContent;
 import cn.edu.thu.log.test.testUI;
 import cn.edu.thu.log.web.service.LogReadService;
+
+
 
 /**
  * Implentation of log reading service
@@ -16,7 +20,6 @@ import cn.edu.thu.log.web.service.LogReadService;
 public class LogReadServiceImpl implements LogReadService {
 	LogConfig logConfig;
 	LogContent logContent;
-
 	/**
 	 * Constructor
 	 */
@@ -25,20 +28,42 @@ public class LogReadServiceImpl implements LogReadService {
 		logContent = new LogContent(logConfig);
 	}
 
-	@Override
-	public ArrayList<Object> readLog(File file, testUI logUI) {
 
+	@Override
+	public ArrayList<Log> readLog(File file, testUI logUI) {
+		
+		
 		// File file=files[0];
 		logConfig.config(file);
 		ArrayList<Object> logTags = logConfig.getLogTags();
 		// read the logFile and display the information on UI
-		logContent.showContent(file, logUI);
+		ArrayList<Log> logList=new ArrayList<Log>();
+		logList=logContent.getContent(file,logUI);
+		if(logList==null){System.out.print("\nlogList  is null");}
+		else{
+		System.out.print("\nthe size of logList:"+logList.size());}
 		// logContent.readFile(file, logUI);
 		// 界面上测试
 		logUI.setTableHead(logTags);
-
-		return null;
+		logUI.setContent(logList);
+		return logList;
 
 	}
+	
+//	@Override
+//	public ArrayList<Object> readLog(File file, testUI logUI) {
+//
+//		// File file=files[0];
+//		logConfig.config(file);
+//		ArrayList<Object> logTags = logConfig.getLogTags();
+//		// read the logFile and display the information on UI
+//		logContent.showContent(file, logUI);
+//		// logContent.readFile(file, logUI);
+//		// 界面上测试
+//		logUI.setTableHead(logTags);
+//
+//		return null;
+//
+//	}
 
 }

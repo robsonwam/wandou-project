e536a2a99ea073697bd2f9716efa927f50829de1
wan254363635@gmail.com <wan254363635@gmail.com@28f5e624-9102-7313-13ba-88312b7d0dff>
2012-02-20 01:47:22 +0000

    加入生成挖掘配置文件代码

---
+++ trunk/PanGu/src/cn/edu/thu/log/web/service/impl/MiningConfigReadServiceImpl.java
@@ -0,0 +1,19 @@
+package cn.edu.thu.log.web.service.impl;
+
+import java.util.ArrayList;
+
+import cn.edu.thu.log.miningconfig.MiningConfigFile;
+import cn.edu.thu.log.web.service.MiningConfigReadService;
+
+public class MiningConfigReadServiceImpl implements MiningConfigReadService{
+	public MiningConfigReadServiceImpl(){
+		
+	}
+
+	@Override
+	public ArrayList<MiningConfigFile> readWebConfig() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}

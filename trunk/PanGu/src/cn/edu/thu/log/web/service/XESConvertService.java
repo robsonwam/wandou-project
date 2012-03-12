5536f341595af67eb8fa6b58f4b970d271f14019
DouMeng1988@gmail.com <DouMeng1988@gmail.com@28f5e624-9102-7313-13ba-88312b7d0dff>
2012-03-12 08:57:30 +0000

    add readXes

--- trunk/PanGu/src/cn/edu/thu/log/web/service/XESConvertService.java
+++ trunk/PanGu/src/cn/edu/thu/log/web/service/XESConvertService.java
@@ -1,14 +1,48 @@
 package cn.edu.thu.log.web.service;
+
+import java.util.Collection;
+
+import org.deckfour.xes.model.XLog;
+
 /**
  * interface of service XES Convert
+ * 
  * @author meng
- *
+ * 
  */
 public interface XESConvertService {
 	/**
 	 * convert logfile to xesfile
-	 * @param readFilePath logFile
-	 * @param resultFile result xes file
+	 * 
+	 * @param readFilePath
+	 *            logFile
+	 * @param resultFile
+	 *            result .xes file
 	 */
-	public void convert(String readFilePath,String resultFile) ;
+	public void convert(String readFilePath, String resultFile);
+
+	/**
+	 * get XLog from converting
+	 * 
+	 * @return XLog which stores the information from .log files
+	 */
+	public XLog getXlog();
+
+	/**
+	 * get list of XLog from a standard log file, like .xes file
+	 * 
+	 * @param standardLogFile
+	 *            the absolute path of xes file
+	 * @return a collection XLogs which store the information from standard log
+	 *         files, like .xes file
+	 */
+	public Collection<XLog> readStandardLog(String standardLogFile);
+
+	/**
+	 * get list of XLog from a standard log file, like .xes file
+	 * 
+	 * @return a collection XLogs which store the information from standard log
+	 *         files, like .xes file
+	 */
+	public Collection<XLog> readStandardLog();
 }
